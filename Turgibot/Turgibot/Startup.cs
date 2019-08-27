18480eb529012d777d3757109c8897b949dd039e
turgibot <turgibot@gmail.com>
2019-08-28 01:52:26 +0300

    create vs project

---
+++ Turgibot/Turgibot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Turgibot.Startup))]
+namespace Turgibot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
